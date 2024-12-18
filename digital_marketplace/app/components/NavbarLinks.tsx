267b0f0447401c392baac1aada6cf4d97c40d78e
ahmet-24810Fun <dragonsfire20201998@gmail.com>
2024-12-18 21:33:01 +0300

    finishing ui

--- digital_marketplace/app/components/NavbarLinks.tsx
+++ digital_marketplace/app/components/NavbarLinks.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export const navbarLinks = [
     {
@@ -27,10 +31,19 @@ export const navbarLinks = [
 
 
 export function NavbarLinks() {
+    
+    const location = usePathname()
+
+   
+    
+
     return(
         <div className="hidden md:flex justify-center items-center col-span-6 gap-x-2">
              {navbarLinks.map((item) => (
-                   <Link href={item.href} key={item.id}>
+                   <Link href={item.href} key={item.id} className={cn(
+                    location === item.href ? 'bg-muted' : 'hover:bg-muted hover:bg-opacity-75',
+                     'group flex items-center px-2 py-2 font-medium rounded-md'
+                   )}>
                     {item.name}
                    </Link>
              ))}
